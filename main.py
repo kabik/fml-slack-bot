ce638699815c15a8bb0bee9997b68610add8267e
kabik <k2.s1k.x20@gmail.com>
2020-01-26 16:28:08 +0900

    Post to slack

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests
+import json
+import os
 from bs4 import BeautifulSoup
 
 TOP_URL = 'https://www.fmylife.com'
 RANDOM_URL = TOP_URL + '/random'
 RETRY = 5
+WEBHOOK_URL = os.environ['WEBHOOK_URL']
 
 for i in range(RETRY):
     html = requests.get(RANDOM_URL).text
     soup = BeautifulSoup(html, 'html.parser')
 
     article = soup.article.select('article a.article-link')[0]
-    article_string = article.string
+    article_string = article.string.strip()
     article_link = TOP_URL + article.get('href')
 
     if article_string: break
 
-print(article)
-print(article_string)
-print(article_link)
+print('article_string = \"{}\"'.format(article_string))
+print('article_link = \"{}\"'.format(article_link))
+
+message_json = {"text": "=== FML ===\n{}\n{}".format(article_string, article_link)}
+res = requests.post(
+    WEBHOOK_URL,
+    json.dumps(message_json),
+    headers={'Content-Type': 'application/json'})
+
+print(res)
